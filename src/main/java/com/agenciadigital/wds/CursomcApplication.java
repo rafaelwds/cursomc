0243a7547de1182875aebfddae4e15b61a0703e4
Rafael Fernando <rafaelfernandoasilva@gmail.com>
2018-06-16 15:24:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/agenciadigital/wds/CursomcApplication.java
+++ src/main/java/com/agenciadigital/wds/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.agenciadigital.wds;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.agenciadigital.wds.domain.Categoria;
+import com.agenciadigital.wds.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+	
+		Categoria cat1 = new Categoria(null,  "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
+	
 }
